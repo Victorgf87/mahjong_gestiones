54f85e7dc8436ebcecb8e66e4ef41240b2da2668
victor <victor.gonzalez@es.fcm.travel>
2025-02-11 16:00:47 +0100

    List of games not done

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,8 @@ class League < ApplicationRecord
   include Imageable
   include Updatable
 
+  has_many :games, as: :event
+
   has_many :event_players, as: :eventable, dependent: :destroy
   has_many :players, through: :event_players
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
