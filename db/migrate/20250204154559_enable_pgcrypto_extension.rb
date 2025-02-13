12b1edf2537b1dcb799c645ad015cecc5a5245e3
Victor Gonzalez <victor.gf87@gmail.com>
2025-02-13 23:55:21 +0100

    Databases per env

--- db/migrate/20250204154559_enable_pgcrypto_extension.rb
+++ db/migrate/20250204154559_enable_pgcrypto_extension.rb
@@ -1,6 +1,8 @@
 class EnablePgcryptoExtension < ActiveRecord::Migration[8.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    unless Rails.env.test?
+      enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    end
     # enable_extension 'pgjwt' unless extension_enabled?('pgjwt')
   end
 end
--- db/migrate/20250210221937_drop_tournament_players.rb
+++ db/migrate/20250210221937_drop_tournament_players.rb
@@ -1,5 +1,5 @@
 class DropTournamentPlayers < ActiveRecord::Migration[8.0]
   def change
-    drop_table :tournament_players
+    drop_table :tournament_players, if_exists: true
   end
 end
