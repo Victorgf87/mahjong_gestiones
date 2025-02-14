ad1703b7eb024e6f35d10de3e99d5b20fce5aeb6
victor <victor.gonzalez@es.fcm.travel>
2025-02-14 14:11:26 +0100

    Edit done

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,4 +1,5 @@
 class LeaguesController < ApplicationController
+
   def index
     @leagues = League.all
   end
@@ -9,6 +10,28 @@ class LeaguesController < ApplicationController
 
   def show
     @league = League.find(params[:id])
+  end
+
+  def edit
+    @league = League.find(params[:id])
+  end
+
+  def update
+    @league = League.find(params[:id])
+    if params[:league][:cover_image_attributes][:file].present?
+      @league.cover_image&.destroy
+    end
+
+    # @league.build_cover_image
+    begin
+      @league.update(league_params)
+    rescue Exception => e
+      @league.update(league_params.except(:cover_image_attributes))
+    end
+
+    assign_players_from_excel(params[:league][:players_file])
+
+    redirect_to league_path(@league)
   end
 
   def create
@@ -24,5 +47,11 @@ class LeaguesController < ApplicationController
 
   def league_params
     params.require(:league).permit(:name, :description, :start_date, :end_date, cover_image_attributes: [ :image_type, :file ])
+  end
+
+  def assign_players_from_excel(data_file)
+    return unless data_file
+    players = process_excel(data_file)
+    @tournament.players = players
   end
 end
