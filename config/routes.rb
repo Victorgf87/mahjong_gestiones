4c1930983ba4950dc9879a7dde6d06fdc507873c
victor <victor.gonzalez@es.fcm.travel>
2025-02-26 13:22:14 +0100

    PWA files

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   resources :players
 
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   resources :tournaments do
     member do
